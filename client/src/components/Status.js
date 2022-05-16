a1ebc15a514c10c93201223fe09f53cdb1b64e12
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-05-16 14:17:10 +0530

    changed styling

--- client/src/components/Status.js
+++ client/src/components/Status.js
@@ -1,43 +1,51 @@
-import React,{useState} from "react";
-import axios from "axios"
-import {Box, Button, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from "@mui/material"
-
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  Box,
+  Button,
+  TextField,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 
 function createData(country, totalConf, totalDeaths, totalRecovered) {
   return { country, totalConf, totalDeaths, totalRecovered };
 }
 
 const Status = () => {
-  let countryName
-  let result
-  const [country, setCountry] = useState("-")
-  const [totalConf, setTotalConf] = useState(0)
-  const [totalDeaths, setTotalDeaths] = useState(0)
-  // const [totalRecov, setTotalRecov] = useState(0)
+  let countryName;
+  let result;
+  const [country, setCountry] = useState("-");
+  const [totalConf, setTotalConf] = useState();
+  const [totalDeaths, setTotalDeaths] = useState();
   const [textValue, setTextValue] = useState("");
-  const onTextChange = (e) => setTextValue(e.target.value) 
+  const onTextChange = (e) => setTextValue(e.target.value);
   const handleSubmit = () => {
-      countryName = textValue
-      async function getStatus(){
-        const Status = await axios.get(`https://api.covid19api.com/summary`)
-        const response = Status.data
-        response.Countries.forEach(element => {
-            if(element.Country === countryName){
-                result = element
-            }
-        });
-        return result
-      }
-      let res = getStatus()
-      res.then(value=>{
-        setCountry(value.Country)
-        setTotalConf(value.TotalConfirmed)
-        setTotalDeaths(value.TotalDeaths)
-        // setTotalRecov(value.TotalRecovered)
-      })
-  }
+    countryName = textValue;
+    async function getStatus() {
+      const Status = await axios.get(`https://api.covid19api.com/summary`);
+      const response = Status.data;
+      response.Countries.forEach((element) => {
+        if (element.Country === countryName) {
+          result = element;
+        }
+      });
+      return result;
+    }
+    let res = getStatus();
+    res.then((value) => {
+      setCountry(value.Country);
+      setTotalConf(value.TotalConfirmed);
+      setTotalDeaths(value.TotalDeaths);
+    });
+  };
   const rows = [
-    createData(country, totalConf, totalDeaths, totalConf-totalDeaths),
+    createData(country, totalConf, totalDeaths, totalConf - totalDeaths),
   ];
   return (
     <>
@@ -58,41 +66,51 @@ const Status = () => {
             onChange={onTextChange}
             value={textValue}
           />
-          <Button variant="contained" onClick={handleSubmit}>
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            sx={{ marginLeft: "20px" }}
+          >
             Submit
           </Button>
         </div>
       </Box>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Country</TableCell>
-            <TableCell align="left">Total Confirmed</TableCell>
-            <TableCell align="left">Total Deaths</TableCell>
-            <TableCell align="left">Total Recovered</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.country}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" sx={{ fontWeight: 700 }}>
+                Country
               </TableCell>
-              <TableCell align="left">{row.totalConf}</TableCell>
-              <TableCell align="left">{row.totalDeaths}</TableCell>
-              <TableCell align="left">{row.totalConf - row.totalDeaths}</TableCell>
+              <TableCell align="center" sx={{ fontWeight: 700 }}>
+                Total Confirmed
+              </TableCell>
+              <TableCell align="center" sx={{ fontWeight: 700 }}>
+                Total Deaths
+              </TableCell>
+              <TableCell align="center" sx={{ fontWeight: 700 }}>
+                Total Recovered
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.country}>
+                <TableCell component="th" scope="row" align="center">
+                  {row.country}
+                </TableCell>
+                <TableCell align="center">{row.totalConf}</TableCell>
+                <TableCell align="center">{row.totalDeaths}</TableCell>
+                <TableCell align="center">
+                  {row.totalConf - row.totalDeaths}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
 
 export default Status;
-
